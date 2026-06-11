9a4ef7b787c34286ec144df147df110c70374626
rounakrkr <rounakkr2017@gmail.com>
2026-06-11 21:41:55 +0530

    Hotfix: remove DB pooling to prevent Serverless DB timeouts

--- backend/models/db.py
+++ backend/models/db.py
@@ -5,18 +5,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Create a connection pool for the database
-db_pool = mysql.connector.pooling.MySQLConnectionPool(
-    pool_name="mineguard_pool",
-    pool_size=5,
-    host=os.getenv("MYSQL_HOST", "localhost"),
-    user=os.getenv("MYSQL_USER", "root"),
-    password=os.getenv("MYSQL_PASSWORD", "12345"),
-    database=os.getenv("MYSQL_DB", "mineguard_db"),
-    port=int(os.getenv("MYSQL_PORT", 3306)),
-    ssl_verify_identity=True
-)
-
 def get_db_connection():
-    """Get a connection from the pool"""
-    return db_pool.get_connection()
+    """Get a fresh connection for each request to avoid serverless timeouts"""
+    return mysql.connector.connect(
+        host=os.getenv("MYSQL_HOST", "localhost"),
+        user=os.getenv("MYSQL_USER", "root"),
+        password=os.getenv("MYSQL_PASSWORD", "12345"),
+        database=os.getenv("MYSQL_DB", "mineguard_db"),
+        port=int(os.getenv("MYSQL_PORT", 3306)),
+        ssl_verify_identity=True
+    )
